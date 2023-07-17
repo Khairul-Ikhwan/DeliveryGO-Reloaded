3fdd6f9b73503c2a65157b65aa1d054e1dcccd7a
Khairul-Ikhwan <khairul.ikhwan00@gmail.com>
2023-07-17 10:47:51 +0800

    Create Navbar

---
+++ src/components/Navbars/DesktopNav.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+import "./navbar.css";
+
+export default function DesktopNav() {
+  return (
+    <div className="container desktop-nav">
+      <p>Book A Delivery</p>
+      <p>About</p>
+      <p>Contact</p>
+      <p>Deliver With Us</p>
+    </div>
+  );
+}
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,10 +1,12 @@
 import DriverLogin from "../../components/Login/DriverLogin";
+import DesktopNav from "../../components/Navbars/DesktopNav";
 import "./App.css";
 
 export default function App() {
   return (
     <main className="App">
-      <h1>App</h1>
+      <DesktopNav />
+      <h1>Powered by the community</h1>
       <DriverLogin />
     </main>
   );
