0aee3df4541266919b32f07b385c50fe72c6d307
Khairul-Ikhwan <khairul.ikhwan00@gmail.com>
2023-07-23 00:59:49 +0800

    Update card css

--- src/pages/Jobs/JobsCard.jsx
+++ src/pages/Jobs/JobsCard.jsx
@@ -34,21 +34,44 @@ export default function JobsCard({ job, onAssignDriver }) {
   return (
     <>
       <div className="job-card">
-        <h3>Job ID:</h3>
-        <p>{id}</p>
-        <p>Job Type: {type_id}</p>
-        <p>
-          Pickup Address: {pickup_address_street}, {pickup_address_unit},{" "}
-          {pickup_address_postal}, {pickup_address_building_name}
-        </p>
-        <p>
-          Delivery Address: {delivery_address_street}, {delivery_address_unit},{" "}
-          {delivery_address_postal}, {delivery_address_building_name}
-        </p>
-        <p>Total Distance: {parseFloat(total_distance).toFixed(1)} km</p>
-        <p>Price: ${parseFloat(price).toFixed(2)}</p>
-        <p>Time: {convertTo12HourFormat(time)}</p>
-        <p>Date: {new Date(date).toLocaleDateString()}</p>
+        <header>
+          <h3>{type_id}</h3>
+          <h3>{new Date(date).toLocaleDateString()}</h3>
+        </header>
+        <span>
+          <h3>Job ID:</h3>
+          <p>{id}</p>
+        </span>
+        <div className="address-field">
+          <p>
+            <h4>Pickup Address:</h4> <br />
+            {pickup_address_street} <br />
+            {pickup_address_unit}, S{pickup_address_postal} <br />
+            {pickup_address_building_name}
+          </p>
+          <p>|</p>
+          <p>
+            <h4>Delivery Address:</h4> <br />
+            {delivery_address_street} <br />
+            {delivery_address_unit}, S{delivery_address_postal} <br />
+            {delivery_address_building_name}
+          </p>
+        </div>
+        <div className="misc-info">
+          <p>
+            Distance: <br />
+            {parseFloat(total_distance).toFixed(1)} km
+          </p>
+          <p>|</p>
+          <p>
+            Price: <br />${parseFloat(price).toFixed(2)}
+          </p>
+          <p>|</p>
+          <p>
+            Time: <br />
+            {convertTo12HourFormat(time)}
+          </p>
+        </div>
         <button onClick={() => onAssignDriver(job.id)}>Accept Job</button>
       </div>
     </>
