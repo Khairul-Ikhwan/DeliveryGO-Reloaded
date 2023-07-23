6343d73b48f84c71c4a740c6b355ee823fd15809
Khairul-Ikhwan <khairul.ikhwan00@gmail.com>
2023-07-23 15:42:14 +0800

    Update CSS and Dash navigation

--- src/pages/Driver/DriverDashboard.jsx
+++ src/pages/Driver/DriverDashboard.jsx
@@ -1,60 +1,14 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { sendRequest } from "../../helpers/send-helper";
-import LogOutButton from "../../components/Login/LogOutButton";
 import "../../styles/dashboard.css";
 import JobsPage from "../Jobs/JobsPage";
+import DriverDetails from "./DriverDetails";
+import DriverNavigation from "./DriverNavigation";
 
 export default function DriverDashboard() {
-  const [driver, setDriver] = useState(null);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const fetchDriverDetails = async () => {
-      try {
-        const token = localStorage.getItem("token");
-        const response = await sendRequest(
-          "/api/drivers/find-driver",
-          "POST",
-          null,
-          {
-            Authorization: `Bearer ${token}`,
-          }
-        );
-        const { driverName, driverEmail, driverPhone, driverPfp } =
-          response.driver;
-        setDriver({ driverName, driverEmail, driverPhone, driverPfp });
-      } catch (error) {
-        console.error("Error fetching driver details:", error);
-        if (error.message === "Network Error") {
-          console.error(
-            "Network error occurred. Check your internet connection."
-          );
-        } else {
-          navigate("/driver");
-        }
-      }
-    };
-
-    fetchDriverDetails();
-  }, [navigate]);
-
-  if (!driver) {
-    return <p>Loading...</p>;
-  }
-
   return (
     <>
-      <div className="dashboard">
-        <img src={driver.driverPfp} alt="Profile" />
-        <h3>Welcome {driver.driverName}!</h3>
-        <p>Email: {driver.driverEmail}</p>
-        <p>Phone: {driver.driverPhone}</p>
-        <LogOutButton path="/driver" />
-      </div>
-      <div>
-        <JobsPage />
-      </div>
+      <DriverDetails />
+      <DriverNavigation />
+      <JobsPage />
     </>
   );
 }
---
+++ src/pages/Driver/DriverNavigation.jsx
@@ -0,0 +1,41 @@
+import { NavLink } from "react-router-dom";
+
+export default function DriverNavigation() {
+  return (
+    <>
+      <div className="navigation-container">
+        <NavLink to="/driver/dashboard">
+          <div className="navigation">
+            <h3>
+              All <br /> Jobs
+            </h3>
+          </div>
+        </NavLink>
+
+        <NavLink to="/driver/dashboard">
+          <div className="navigation">
+            <h3>
+              Accepted <br /> Jobs
+            </h3>
+          </div>
+        </NavLink>
+
+        <NavLink to="/driver/dashboard">
+          <div className="navigation">
+            <h3>
+              Completed <br /> Jobs
+            </h3>
+          </div>
+        </NavLink>
+
+        <NavLink to="/driver/dashboard">
+          <div className="navigation">
+            <h3>
+              My <br /> Profile
+            </h3>
+          </div>
+        </NavLink>
+      </div>
+    </>
+  );
+}
